87191130ea962dba8a843e2e2595debfb644e198
Evan McCarthy <emccarthy3@washcoll.edu>
2021-11-18 16:10:11 -0500

    Push

---
+++ program_1.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+# ------------------------------
+# Ambiguous Documents
+# Software Engineering
+# Coding Project: Recreating IM Leagues
+# Evan McCarthy, Jakob Watt, Tegan McBride, Jack Maher, Christian Gruyon
+# ------------------------------
+
+top = Tk()
+top.title("Sports for the less sporty")
+#setup fancy windows
+
+
+def button_click(number):
+    return
+
+## Evan Add
+button_1 = Button(top, text="1", padx=40, pady=20, command=lambda: button_click(1))
+
+button_1.grid(row=0, column=0)
+
+welcome_label = 
+
+## Evan Add End
+
+top.mainloop()
