5cb5cb660d50ff5f398001a659c1e384ef251254
Evan McCarthy <emccarthy3@washcoll.edu>
2021-11-22 19:15:45 -0500

    Team change players

--- Notebook Demo.py
+++ Notebook Demo.py
@@ -13,31 +13,38 @@ def forgetWig(wig):
     for widget in wig.winfo_children():
         widget.destroy()
 
-def changeFrame(frame):
-    forgetWig(currentFrame)
-    lbl_Players = Label(currentFrame, text="Players:")
-    if frame == "button_1":    
-        button_a1 = Button(currentFrame, text="Sam")
-        button_a2 = Button(currentFrame, text="Jack")
-    elif frame == "button_2":
-        button_a1 = Button(currentFrame, text="Megan")
-        button_a2 = Button(currentFrame, text="Lobo")
-    elif frame == "button_3":
-        button_a1 = Button(currentFrame, text="Jack")
-        button_a2 = Button(currentFrame, text="Jill")
-    elif frame == "button_4":
-        button_a1 = Button(currentFrame, text="Aaron")
-        button_a2 = Button(currentFrame, text="Jim")
+def changeFrame(frame): ###TODO: Need to add another change frame inside to show players when each team is selected
+    #forgetWig(currentFrame)
+    tempFrame = Frame(notebook)
+    lbl_Players = Label(tempFrame, text="Teams:")
+    if frame == "Tennis":    
+        button_a1 = Button(tempFrame, text="Team 1")
+        button_a2 = Button(tempFrame, text="Team 2")
+    elif frame == "Kickball":
+        button_a1 = Button(tempFrame, text="Team 1")
+        button_a2 = Button(tempFrame, text="Team 2")
+    elif frame == "Basketball":
+        button_a1 = Button(tempFrame, text="Team 1")
+        button_a2 = Button(tempFrame, text="Team 2")
+    elif frame == "Soccer":
+        button_a1 = Button(tempFrame, text="Team 1")
+        button_a2 = Button(tempFrame, text="Team 2")
     
-    lbl_Players.pack() #Pack this first to be on top
+    lbl_Players.grid(row=0, column=0) #Pack this first to be on top
     #-------Expand this list?
-    button_a1.pack()
-    button_a2.pack()
+    button_a1.grid(row=1, column=0)
+    button_a2.grid(row=2, column=0)
     #--------Do these after
-    lbl_bottom = Label(currentFrame, text="^Look its player buttons")
-    lbl_bottom.pack()
-    notebook.add(currentFrame, text='Current Frame')
-    notebook.select(1)#DONT LIKE
+    lbl_bottom = Label(tempFrame, text="^Look its Team buttons")
+    lbl_bottom.grid(row=3, column=0)
+    for i in range(len(tabFrameList)):
+        print(tabFrameList[i])
+        if frame == tabFrameList[i][0]:
+            notebook.select(tabFrameList[i][1])
+            return
+    tabFrameList.append([frame, tempFrame])
+    notebook.add(tempFrame, text=frame)
+    notebook.select(tempFrame)
 
 top = Tk() #needs to be on top of other code, similar to main()
 top.title("Lazie Sports")
@@ -49,15 +56,17 @@ topFrame = Frame(notebook)
 
 currentFrame = Frame(notebook, borderwidth = 2, bg="Black")
 #teamlist = []
-teamlist = ["WMT", "IML", "IMT", "Testing the length?"]
+sportList = ["Tennis", "Kickball", "Basketball", "Soccer"]
 
-list_Team1 = ["Sam", "Jack", "John"]
-list_Team2 = ["Megan", "Austin", "Allie"]
+list_Tennis = ["WMT", "IML", "TMP"]
+list_Kickball = ["WMT", "IML", "TMP"]
 
-button_1 = Button(topFrame, text=teamlist[0], padx=40, pady=20, command=lambda: changeFrame("button_1"))
-button_2 = Button(topFrame, text=teamlist[1], padx=40, pady=20, command=lambda: changeFrame("button_2"))
-button_3 = Button(topFrame, text=teamlist[2], padx=40, pady=20, command=lambda: changeFrame("button_3"))
-button_4 = Button(topFrame, text=teamlist[3], padx=40, pady=20, command=lambda: changeFrame("button_4"))
+tabFrameList = []#list for holding tabs to refrence later
+
+button_1 = Button(topFrame, text=sportList[0], padx=40, pady=20, command=lambda: changeFrame(sportList[0]))
+button_2 = Button(topFrame, text=sportList[1], padx=40, pady=20, command=lambda: changeFrame(sportList[1]))
+button_3 = Button(topFrame, text=sportList[2], padx=40, pady=20, command=lambda: changeFrame(sportList[2]))
+button_4 = Button(topFrame, text=sportList[3], padx=40, pady=20, command=lambda: changeFrame(sportList[3]))
 
 
 button_1.grid(row=1, column=0)
@@ -65,7 +74,7 @@ button_2.grid(row=2, column=0)
 button_3.grid(row=3, column=0)
 button_4.grid(row=4, column=0)
 
-welcome_label = Label(topFrame, text="Teams") #"Changed Washing Machine Team" to "Teams"
+welcome_label = Label(topFrame, text="Sports") #"Changed Washing Machine Team" to "Teams"
 welcome_label.grid(row=0, column=0)
 
 notebook.add(topFrame, text='Home')
