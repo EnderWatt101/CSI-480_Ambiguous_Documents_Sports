de2c42d1d8b7e76f4d2d76c84fddacdf71cc1038
Evan McCarthy <emccarthy3@washcoll.edu>
2021-12-02 17:33:30 -0500

    Tabs!

--- Team full tabs.py
+++ Team full tabs.py
@@ -10,7 +10,7 @@ from tkinter import ttk
 class sport():
     def __init__(self, sportNameIn):
         self.sportName = sportNameIn
-        self.teamList = [team("Team 1"), team("Team 2")]
+        self.teamList = [team("Team 1"), team("Team 2"), team("Bobcats")]
     def addTeam(self, team):
         self.teamList.append(team)
     def removeTeam(self, teamName):
@@ -27,7 +27,7 @@ class sport():
 class team():
     def __init__(self, teamNameIn):
         self.teamName = teamNameIn
-        self.playerList = [player("Player 1"), player("Player 2")]
+        self.playerList = [player("Player 1"), player("Player 2"), player("Ann")]
     def addPlayer(self,player):
         self.playerList.append(player)
     def removePlayer(self, playerName):
@@ -48,6 +48,7 @@ class player():
         return self.playerName
 
 
+
 ##SPORTDATA
 
 tennis = sport("Tennis")
@@ -64,8 +65,8 @@ def sportTabPress(sportNameIn): ###TODO: Need to add another change frame inside
     sportNotebook = ttk.Notebook(notebook)
     notebook.add(sportNotebook, text=thisSportName)
     notebook.select(sportNotebook)
-    tempFrame = Frame(sportNotebook)
-    title = Label(tempFrame, text="Teams:")
+    tempFrame = Frame(sportNotebook, width= 800, height=200)
+    title = Label(tempFrame, text="Teams:", font=("Arial",25))
     title.pack()
     for i in range(len(sportData)):#Each sport
         print(sportData[i].getSportName())
@@ -73,7 +74,7 @@ def sportTabPress(sportNameIn): ###TODO: Need to add another change frame inside
         if thisSportName == sportData[i].getSportName():
             print("gether")
             for j in range(len(sportData[i].getTeamList())): #Each Team
-                tempButton = Button(tempFrame, text=sportData[i].getTeamList()[j].getTeamName(), command=lambda i=i, j=j:teamButtonPress(sportData[i].getSportName(),sportData[i].getTeamList()[j].getTeamName())) #Add command
+                tempButton = Button(tempFrame, text=sportData[i].getTeamList()[j].getTeamName(), command=lambda i=i, j=j:teamButtonPress(sportData[i].getSportName(),sportData[i].getTeamList()[j].getTeamName()), font=("Arial",15)) #Add command
                 tempButton.pack()
             #this should only exit for loop and go through rest of func
 
@@ -99,9 +100,9 @@ def teamButtonPress(sportNameIn, teamNameIn):#TODO FIX ALL THIS
         if sportNameIn == tabFrameList[i][0]:
             internalNotebook = tabFrameList[i][1]
             break
-    tempFrame = Frame(internalNotebook)
+    tempFrame = Frame(internalNotebook, width= 800, height=400)
 
-    title = Label(tempFrame, text="Players:")
+    title = Label(tempFrame, text="Players:", font=("Arial",25))
     title.pack()
 
 
@@ -110,7 +111,7 @@ def teamButtonPress(sportNameIn, teamNameIn):#TODO FIX ALL THIS
             for j in range(len(sportData[i].getTeamList())):
                 if teamNameIn == sportData[i].getTeamList()[j].getTeamName():
                     for k in range(len(sportData[i].getTeamList()[j].getPlayerList())):
-                        tempButton = Button(tempFrame, text=sportData[i].getTeamList()[j].getPlayerList()[k].getPlayerName())
+                        tempButton = Button(tempFrame, text=sportData[i].getTeamList()[j].getPlayerList()[k].getPlayerName(), font=("Arial",15))
                         tempButton.pack()
 
 
@@ -125,10 +126,10 @@ top.title("Lazie Sports")
 
 notebook = ttk.Notebook(top)
 notebook.pack(pady=10, expand=True)
-topFrame = Frame(notebook)
+topFrame = Frame(notebook, width= 800, height=400)
 
 
-currentFrame = Frame(notebook, borderwidth = 2, bg="Black")
+currentFrame = Frame(notebook, borderwidth = 2, bg="Black", width= 800, height=400)
 #teamlist = []
 sportList = ["Tennis", "Kickball", "Basketball", "Soccer"]
 sportDict = {"Tennis": [{}, "ten2", "ten3"], "Kickball": ["kick1", "kick2", "kick3"], "Basketball": ["bask1", "bask2", "bask3"], "Soccer": ["socc1", "socc2", "socc3"]}
@@ -142,26 +143,14 @@ buttonList = []
 
 for i in range(len(sportData)):#Each sport
     print("each sport button: " + sportData[i].getSportName())
-    buttonList.append(Button(topFrame, text=sportData[i].getSportName(), command=lambda i=i: sportTabPress(sportData[i].getSportName())).grid(row=i+1, column=0))
+    buttonList.append(Button(topFrame, text=sportData[i].getSportName(), command=lambda i=i: sportTabPress(sportData[i].getSportName()), font=("Arial",15)).grid(row=i+1, column=0))
 
 
 
 
-#button_1 = Button(topFrame, text=sportList[0], padx=40, pady=20, command=lambda: changeFrame(sportList[0]))
-#button_2 = Button(topFrame, text=sportList[1], padx=40, pady=20, command=lambda: changeFrame(sportList[1]))
-#button_3 = Button(topFrame, text=sportList[2], padx=40, pady=20, command=lambda: changeFrame(sportList[2]))
-#button_4 = Button(topFrame, text=sportList[3], padx=40, pady=20, command=lambda: changeFrame(sportList[3]))
-
-
-#button_1.grid(row=1, column=0)
-#button_2.grid(row=2, column=0)
-#button_3.grid(row=3, column=0)
-#button_4.grid(row=4, column=0)
-
-welcome_label = Label(topFrame, text="Sports") #"Changed Washing Machine Team" to "Teams"
+welcome_label = Label(topFrame, text="Sports", font=("Arial",25)) #"Changed Washing Machine Team" to "Teams"
 welcome_label.grid(row=0, column=0)
 
 notebook.add(topFrame, text='Home')
-#notebook.add(frame2, text='Profile')
 
 top.mainloop()
